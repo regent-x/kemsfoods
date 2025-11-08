99c88826ae225385b082bf25f7dea7bbb8282efb
regent-x <spiritofdamola@gmail.com>
2025-11-08 16:28:26 +0000

    issue: NavBar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
